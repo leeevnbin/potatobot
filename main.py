870ee4651230b5bb01166ee567b32dbc7740e0a3
eunbin <leeevnbin@gmail.com>
2025-11-25 15:51:14 +0900

    [feat] 사용자가 채널 참여 시 봇 인사말 보내기

--- main.py
+++ main.py
@@ -13,7 +13,24 @@ app = App(token=os.environ["SLACK_BOT_TOKEN"])
 @app.message("감자야")
 def say_hello(message, say):
     user = message["user"]
+    channel = message["channel"]
+
+    if channel != "C09V873SXG9":
+        return None
+
     say(f"안녕, <@{user}>! 나는 찜감자야. 수줍..")
+
+
+@app.event("member_joined_channel")
+def handle_channel_bot_greeting(event, say):
+    user = event["user"]
+    channel = event["channel"]
+
+    if channel != "C09V0J81W1L":
+        return None
+
+    text = f"안녕, <@{user}>! 나는 소식해서 시무룩한 찜감자봇이라고 해 :찜감자:"
+    say(channel=channel, text=text)
 
 
 @app.event("emoji_changed")
@@ -27,18 +44,14 @@ def handle_emoji_changed_command(event, client: WebClient):
         text = f"감자농장에 `{emoji_name}`가 이사왔습니다."
 
         client.chat_postMessage(
-            channel="C09V873SXG9",
+            channel="C09V0J81W1L",
             text=text,
             blocks=[
+                {"type": "image", "image_url": emoji_url, "alt_text": emoji_name},
                 {
                     "type": "section",
                     "text": {"type": "mrkdwn", "text": text},
-                    "accessory": {
-                        "type": "image",
-                        "image_url": emoji_url,
-                        "alt_text": emoji_name,
-                    },
-                }
+                },
             ],
         )
 
